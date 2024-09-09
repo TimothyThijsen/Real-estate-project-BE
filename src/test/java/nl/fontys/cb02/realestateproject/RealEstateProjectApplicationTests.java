7f808e43302129c7073fbadc6eca5a5f16bfca2f
Timothy Thijsen <t.thijsen@fontys.nl>
2024-09-09 16:07:32 +0200

    First commit, and project setup

---
+++ src/test/java/nl/fontys/cb02/realestateproject/RealEstateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.cb02.realestateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

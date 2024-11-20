c6812089edc8a6ca4d9105efbce21f37eae60ade
Timothy Thijsen <504835@student.fontys.nl>
2024-11-20 13:54:31 +0100

    Added new qeury to propertyRepo

--- src/main/java/nl/fontys/realestateproject/business/impl/PropertyServiceImpl.java
+++ src/main/java/nl/fontys/realestateproject/business/impl/PropertyServiceImpl.java
@@ -91,7 +91,7 @@ public class PropertyServiceImpl implements PropertyService {
 
     @Override
     public GetAllPropertiesResponse getAllProperties() {
-        List<PropertyEntity> results = propertyRepository.findAll();
+        List<PropertyEntity> results = propertyRepository.findAllAvailableProperty();
 
         final GetAllPropertiesResponse response = new GetAllPropertiesResponse();
         List<Property> properties = results
--- src/main/java/nl/fontys/realestateproject/persistence/PropertyRepository.java
+++ src/main/java/nl/fontys/realestateproject/persistence/PropertyRepository.java
@@ -2,9 +2,12 @@ package nl.fontys.realestateproject.persistence;
 
 import nl.fontys.realestateproject.persistence.entity.PropertyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface PropertyRepository extends JpaRepository<PropertyEntity, Long> {
-
-
+    @Query("SELECT p FROM PropertyEntity p left join  TransactionEntity t on p.id = t.propertyId where t.propertyId is null")
+    List<PropertyEntity> findAllAvailableProperty();
 }
